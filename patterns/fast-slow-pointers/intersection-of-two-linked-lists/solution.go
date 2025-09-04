16b9b7003a05b2637ad1161942b602ac17650111
Steve Morgan <73501129+rebelopsio@users.noreply.github.com>
2025-09-04 17:00:37 -0400

    problem(intersection-of-two-linked-lists): solved in go

--- patterns/fast-slow-pointers/intersection-of-two-linked-lists/solution.go
+++ patterns/fast-slow-pointers/intersection-of-two-linked-lists/solution.go
@@ -6,19 +6,38 @@ import "fmt"
 // Pattern: fast-slow-pointers
 // LeetCode: 160
 
-func solve() {
-    // TODO: Implement solution
-    fmt.Println("Solution not implemented yet")
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	ptr_a, ptr_b := headA, headB
+	for ptr_a != ptr_b {
+		if ptr_a != nil {
+			ptr_a = ptr_a.Next
+		} else {
+			ptr_a = headB
+		}
+		if ptr_b != nil {
+			ptr_b = ptr_b.Next
+		} else {
+			ptr_b = headA
+		}
+	}
+	return ptr_a
 }
 
 func main() {
-    solve()
+	solve()
 }
 
 // Test cases
 func runTests() {
-    // TODO: Add test cases
-    fmt.Println("Tests not implemented yet")
+	// TODO: Add test cases
+	fmt.Println("Tests not implemented yet")
 }
 
 /*
